0ea3d56c0c8c13b964d87350762835ec3f354e95
Kiril Popov <kkpopov@hotmail.com>
2021-04-15 15:49:37 +0300

    update so apps auto join channels

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const doWhenGlueReady = (glue) => {
+const doWhenGlueReady = async (glue) => {
     window.glue = glue;
 
-    glue.channels.subscribe((data) => {
+    glue.channels.subscribe((data, a, b) => {
+        console.log(data, a, b)
         visualizeChannelsContext(JSON.stringify(data, null, 2));
     });
+
+    // instead of doing setTimeouts, we should get glue.channels.my() promise version
+    // to ask for the current channel
+    setTimeout(async () => {
+        const onChannel = glue.channels.current() !== undefined;
+        console.log(`current is`, glue.channels.current());
+        if (!onChannel) {
+            //join random channel if not
+            let channel = glue.windows.my().context.channel;
+            if (!channel) {
+                const allChannels = await glue.channels.all();
+                channel = allChannels[Math.floor(Math.random() * allChannels.length)];
+            }
+            glue.channels.join(channel);
+        }
+    }, 100);
+
 }
 
 const visualizeAppName = (appName) => {
@@ -22,6 +40,12 @@ const updateChannel = () => {
     glue.channels.publish(update);
 }
 
+const openApp = () => {
+    // order random app on the same channel
+    const apps = ["orders", "orders-history", "positions", "order-ticket"];
+    const appToOpen = apps[Math.floor(Math.random() * apps.length)];
+    glue.appManager.application(appToOpen).start({ channel: glue.channels.current() });
+}
 
 Glue({ channels: true }).then(doWhenGlueReady);
 visualizeAppName(glue42gd.application);

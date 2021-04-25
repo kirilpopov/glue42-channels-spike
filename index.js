91419ae0e4c0045e2d209ff775dc842b02d2451d
Kiril Popov <kkpopov@hotmail.com>
2021-04-25 09:18:39 +0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const doWhenGlueReady = async (glue) => {
     window.glue = glue;
 
+    const myApp = glue.config.application;
+    setAppToOpen(myApp);
+
+    //aby: adding code to save additional data when saving layout
+    glue.layouts.onSaveRequested((requestInfo) => {
+        // Determine the layout type.
+        const layoutType = requestInfo.layoutType;
+
+        // store context only if global layouts
+        if (layoutType === "Global") {
+            return { windowContext: { myname: glue42gd.application, channelId: glue.channels.my(), layoutName: cbData.layoutName }, activityContext: {} }
+        }
+    });
+
+    //aby: let us read in the data that was saved while saving the layout
+    const myWin = glue.windows.my();
+    myWin.getContext().then((windowCtx) => {
+        document.getElementById("layout-context").innerHTML = JSON.stringify(windowCtx);
+    });
+
+    // subscribe for channel changes
     glue.channels.subscribe((data, a) => {
         visualizeChannelsContext(JSON.stringify(data, null, 2));
     });
 
-    // instead of doing setTimeouts, we should get glue.channels.my() promise version
-    // to ask for the current channel
-    setTimeout(async () => {
-        const onChannel = glue.channels.current() !== undefined;
-        if (!onChannel) {
-            //join random channel if not on a channel
-            const allChannels = await glue.channels.all();
-            channel = allChannels[Math.floor(Math.random() * allChannels.length)];
-            glue.channels.join(channel);
-        }
-    }, 100);
-
+    // if not started on some channel, join a random channel
+    const onChannel = glue.channels.current() !== undefined;
+    if (!onChannel) {
+        joinRandomChannel(glue);
+    }
 }
 
 const visualizeAppName = (appName) => {
@@ -35,12 +49,41 @@ const updateChannel = () => {
     glue.channels.publish(update);
 }
 
-const openApp = () => {
-    // order random app on the same channel
-    const apps = ["orders", "orders-history", "positions", "order-ticket"];
-    const appToOpen = apps[Math.floor(Math.random() * apps.length)];
-    glue.appManager.application(appToOpen).start({}, { channelId: glue.channels.current() });
+const setAppToOpen = (myApp) => {
+    if (myApp === "orders") {
+        appToOpenFromCurrent = "orders-history";
+    } else if (myApp === "positions") {
+        appToOpenFromCurrent = "order-ticket";
+    }
+    if (!appToOpenFromCurrent) {
+        document.getElementById("openAppButton").setAttribute("disabled", "");
+    }
 }
 
+const openApp = () => {
+    glue.appManager.application(appToOpenFromCurrent).start({}, { channelId: glue.channels.current() });
+}
+
+const joinRandomChannel = async (glue) => {
+    // get a list of all running windows that are on some channel
+    let windowsWithChannels = await glue.channels.getWindowsWithChannels();
+    // get all available channels
+    const allChannels = await glue.channels.all();
+    // find all free channels 
+    let freeChannels = allChannels.filter((c) => {
+        // if the channel is already on some window - it's not free
+        return !windowsWithChannels.find((i) => i.channel === c);
+    });
+    // if no free , use all
+    if (freeChannels.length === 0){
+        freeChannels = allChannels;
+    }
+
+    //join random channel from the free channels
+    channel = freeChannels[Math.floor(Math.random() * freeChannels.length)];
+    glue.channels.join(channel);
+}
+
+let appToOpenFromCurrent;
 Glue({ channels: true }).then(doWhenGlueReady);
 visualizeAppName(glue42gd.application);
